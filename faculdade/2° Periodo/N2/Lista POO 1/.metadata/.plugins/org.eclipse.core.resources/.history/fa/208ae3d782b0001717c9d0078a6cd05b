01981daaad4aa2cc818a8448af49fd5c1388afef
renatosi <renatooliveirasi@hotmail.com>
2017-10-13 23:43:25 -0300

    *Exercicio 1 e 3 prontas 2 em andamento

---
+++ faculdade/2° Periodo/N2/Lista POO 1/.metadata/.plugins/org.eclipse.core.resources/.history/fa/208ae3d782b0001717c9d0078a6cd05b
@@ -0,0 +1,5 @@
+package br.com.MetodoVariavel;
+
+public class MetodoVariavel {
+
+}
---
+++ faculdade/2° Periodo/N2/Lista POO 1/ListadePOO/src/br/com/Ex_01/Objeto_Principal.java
@@ -0,0 +1,35 @@
+package br.com.Ex_01;
+
+import java.util.Scanner;
+
+public class Objeto_Principal {
+
+	public static void main(String[] args) {
+
+			Scanner leitura = new Scanner(System.in);
+			
+			System.out.print("Digite o inicio: ");
+			int inicio = leitura.nextInt();
+			System.out.print("Digite o fim: ");
+			int fim = leitura.nextInt();
+			System.out.print("Digite o numero [A]: ");
+			int a = leitura.nextInt();
+			System.out.print("Digite o numero [B]: ");
+			int b = leitura.nextInt();
+			Objeto o1 = new Objeto(inicio, fim, a, b);
+			
+			System.out.println(o1.Hello());
+			
+			System.out.println(o1.getImpares());
+			
+			leitura.nextLine();
+			
+			System.out.println("String invertida: \n"
+			+ o1.inverterString("BANANA")
+			+"\nA pontenicia entre Inicio e Fim e: "+o1.Operacao1()
+			+"\nO calculo a² + 2*a*b+b² e: "+o1.Operacao2());
+			
+		}
+
+	}
+
