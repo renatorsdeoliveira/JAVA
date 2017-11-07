445c707c26ec97ce2824b1f2730fb45071077bdc
renatosi <renatooliveirasi@hotmail.com>
2017-11-07 18:49:15 -0200

    Organizando

---
+++ faculdade/2° Periodo/N2/Lista POO 2/src/br/com/Ex_03/Pessoa.java
@@ -0,0 +1,38 @@
+package br.com.Ex_03;
+
+public class Pessoa {
+
+	private String nome;
+	private String numero;
+	
+	public Pessoa(String nome, String numero) {
+		super();
+		this.nome = nome;
+		this.numero = numero;
+	}
+
+	public Pessoa() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+	
+	
+	
+}
